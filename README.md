fac28cb3000d9515820cbdf7ec6bb80c07f14287
bmink <barnamink@gmail.com>
2025-03-31 23:17:44 -0700

    .

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # esp_rotary
+
+Rock solid, accurate & efficient rotary encoder driver for ESP32. Will not
+miss or count extra steps even when turned very fast.
+
+Features:
+* Counting is done with a state machine to ensure reliable increment /
+decrement counting
+* Multiple rotary count modes: "infinite", bound, wraparound
+* Optional "speed boost" mode: if the knob is turned fast, a multiplier is
+applied to the increments / decrements
+* Callers receive turn and switch events via a queue *and* also can read
+count value and switch status at any time
+* Switch implements software debounce
+
+## Using the driver
+
+### Setup
+
+### Encoder events and reading values
+
+
+## Implementation notes
+
+### Rotary encoder state machine
+
+
--- main/esp_rotary.c
+++ main/esp_rotary.c
@@ -6,6 +6,20 @@
 #include "soc/soc.h"
 #include "soc/gpio_reg.h"
 #include "esp_check.h"
+#include "esp_rotary.h"
+
+
+typedef struct rotary {
+	rotary_config_t	ro_config;
+	unsigned char	ro_state;
+	
+	QueueHandle_t 	ro_value_mailb;
+	QueueHandle_t 	ro_switch_state_mailb;
+	
+} rotary_t;
+
+static rotary_t	*rotary = NULL;
+static unsigned char rotary_cnt = 0;	
 
 static const char *logtag = "rotary";
 
@@ -25,8 +39,8 @@ static const char *logtag = "rotary";
  * We could do a time-based debounce, which could work but at higher spin
  * speeds would start to miss steps.
  *
- * A better idea (not mine) is to take advantage of the fact that we *do* have
- * two inputs that go active/inactive, offset from one another, and in a
+ * A better idea is to take advantage of the fact that we *do* have two
+ * inputs that go active/inactive, offset from one another, and in a
  * pattern that's unique for the CW / CCW directions. This means that we can
  * implement a state machine to go through the pin value stages of CW or CCW
  * rotation in order.
@@ -50,13 +64,13 @@ static const char *logtag = "rotary";
  * We start in IDLE, when both pins are inactive. When the rotary encoder is
  * turned, the state machine will start advancing in the direction indicated by 
  * the pin values. Due to bounce, during each state change it may oscillate
- * back and forth between the states a few times.
+ * back and forth between the states a number of times.
  * 
  * When a full go-around has been completed, the step increment
  * or decrement is noted. What's most important to understand is that the steps
  * CW3->IDLE and CCW3->IDLE *only* result in an increment / decrement 
  * respectively, if a *whole* loop has been completed. Temporary bounce back
- * and forth between IDLE and CW3/CCW3 will not result in the counter changing.
+ * and forth between IDLE and CW1/CCW1 will not result in the counter changing.
  * 
  * If at any point in the state machine we encounter an input that's not
  * allowed for the state we are in, we ignore that input except for HH, in
@@ -64,54 +78,107 @@ static const char *logtag = "rotary";
  * back to a known state if somehow the steps or signals got messed up.
  */
 
-enum rotary_state {
-	STATE_IDLE,
-	CW_1,
-	CW_2,
-	CW_3,
-	CCW_1,
-	CCW_2,
-	CCW_3
-}
+#define STATE_IDLE	0
+#define STATE_CW1	1
+#define STATE_CW2	2
+#define STATE_CW3	3
+#define STATE_CCW1	4
+#define STATE_CCW2	5
+#define STATE_CCW3	6
+#define STATE_CNT	7
+
+static unsigned char rot_state = STATE_IDLE;
 
 
-enum event_type {
-	EVENT_TURN_CLOCKW,
-	EVENT_TURN_COUNTW,
-	EVENT_SWITCH_CLOSE,
-	EVENT_SWITCH_OPEN,
+/* rot_state_change contains the new state for each possible input for each
+ * possible state. Each row represents the corresponding state value.
+ * The columns in each state represent the new state based on the value of
+ * A and B in the format: A << 1 + B. So the first column is the new state if
+ * the input is LOW/LOW (ie. both A and B active) and the fourth column
+ * represents HIGH/HIGH (both inactive). The format of the new state byte
+ * is as follows:
+ *  7 6 5 4 3 2 1 0
+ *  | | | | | | | +- bits 0-4 represent the index of the new state
+ *  | | | | | | +--- see above
+ *  | | | | | +----- see above
+ *  | | | | +------- see above
+ *  | | | +--------- not used
+ *  | | +----------- not used
+ *  | +------------- decrement counter
+ *  +--------------- increment counter
+ */
+
+#define COUNT_INCR	1 << 7
+#define COUNT_DECR	1 << 6
+
+static char rot_state_change[STATE_CNT][4] = {
+
+       /* STATE_IDLE 
+        * LL            LH              HL              HH                  */
+	{ STATE_IDLE,	STATE_CW1, 	STATE_CCW1,	STATE_IDLE },
+
+       /* STATE_CW1 
+        * LL            LH              HL              HH                  */
+	{ STATE_CW2,	STATE_CW1, 	STATE_CW1,	STATE_IDLE },
+
+       /* STATE_CW2 
+        * LL            LH              HL              HH                  */
+	{ STATE_CW2,	STATE_CW1, 	STATE_CW3,	STATE_IDLE },
+
+       /* STATE_CW3 
+        * LL            LH              HL              HH                  */
+	{ STATE_CW2,	STATE_CW3, 	STATE_CW3,	STATE_IDLE|COUNT_INCR },
+
+       /* STATE_CCW1 
+        * LL            LH              HL              HH                  */
+	{ STATE_CCW2,	STATE_CCW1, 	STATE_CCW1,	STATE_IDLE },
+
+       /* STATE_CCW2 
+        * LL            LH              HL              HH                  */
+	{ STATE_CCW2,	STATE_CCW3, 	STATE_CCW1,	STATE_IDLE },
+
+       /* STATE_CCW3 
+        * LL            LH              HL              HH                  */
+	{ STATE_CCW2,	STATE_CCW3, 	STATE_CCW3,	STATE_IDLE|COUNT_DECR }
 };
 
 
+/* Internal event queue. Used by the ISRs to record events as they see them
+ *
+ * event is a single byte of the following format:
+ *  7 6 5 4 3 2 1 0
+ *  | | | | | | | +- status of rotary B
+ *  | | | | | | +--- status of rotary A
+ *  | | | | | +----- status of switch (0=open, 1=closed)
+ *  | | | | +------- not used
+ *  | | | +--------- not used
+ *  | | +----------- not used
+ *  | +------------- not used
+ *  +--------------- event type (1 - rotary, 0 - switch)
+ */
 #define EVENT_QUEUE_SIZ	10
 static QueueHandle_t _event_queue = NULL;
 
+static int rot_value;
+
+
 void
-isr_rotary_a(void *arg)
+isr_rotary(void *arg)
 { 
-
-#if 0
-	int	rotary_idx;
-
-	rotary_idx = (uint32_t) arg;
-#endif
-
-
+	uint32_t a_value;
 	uint32_t b_value;
-	uint32_t ev;
+	uint32_t regs;
+	char event;
 
-	/* Since we are in an ISR, we are checking the B value by accessing
-	 * the GPIO input register directly
-	 * Alternatively could set CONFIG_GPIO_CTRL_FUNC_IN_IRAM in the config,
-	 * then presumably we could use gpio_get_level() (? need to verify) */
-	b_value = (REG_READ(GPIO_IN_REG) >> GPIO_ROT_B) & 1;
+	/* Since we are in an ISR, we are checking the pin value by accessing
+	 * the GPIO input register directly */
+	regs = (REG_READ(GPIO_IN_REG));
+	a_value = (regs >> GPIO_ROT_A) & 1;
+	b_value = (regs >> GPIO_ROT_B) & 1;
 
-	if(b_value)
-		ev = EVENT_TURN_CLOCKW;
-	else
-		ev = EVENT_TURN_COUNTW;
+	event = (a_value << 1) | b_value | (1 << 7);
 
-	xQueueSendFromISR(_event_queue, &ev, NULL);
+	xQueueSendFromISR(_event_queue, &event, NULL);
 }
 
 
@@ -124,6 +191,43 @@ isr_rotary_switch(void *arg)
 	rotary_idx = (uint32_t) arg;
 #endif
 }
+
+
+static void event_loop(void *arg)
+{
+	char	event;
+	char	statech;
+
+printf("Rotary size: %d\n", sizeof(esp_rotary_t));
+printf("Entering event loop\n");
+	while(1) {
+		if(xQueueReceive(_event_queue, &event, portMAX_DELAY)) {
+			if(event & (1<<7)) {
+				/* Rotary event */
+
+				
+				statech =
+				    rot_state_change[rot_state][event & 0xf];
+
+				rot_state = statech & 0xf;
+				if(statech & COUNT_INCR)
+					++rot_value;
+				else
+				if(statech & COUNT_DECR)
+					--rot_value;
+
+				if(statech > 0xf)
+					printf("rot_value = %d\n", rot_value);
+			}
+
+			
+		}
+		//printf("high watermark: %d\n",
+		//    uxTaskGetStackHighWaterMark(NULL));
+	}
+}
+
+
   
 
 int
@@ -135,7 +239,7 @@ config_gpio(int pinnr, int isinput, gpio_isr_t isr, void *isr_arg)
 	memset(&config, 0, sizeof(gpio_config_t));
 
 	config.mode = isinput ? GPIO_MODE_INPUT : GPIO_MODE_OUTPUT;
-	config.intr_type = isr ? GPIO_INTR_NEGEDGE : GPIO_INTR_DISABLE;
+	config.intr_type = isr ? GPIO_INTR_ANYEDGE : GPIO_INTR_DISABLE;
     	config.pin_bit_mask = 1ULL << pinnr;
 	config.pull_up_en = 1;
 	ret = gpio_config(&config);
@@ -156,32 +260,8 @@ config_gpio(int pinnr, int isinput, gpio_isr_t isr, void *isr_arg)
 	return 0;
 }
 
+
 #define EVENT_LOOP_PRIORITY	10
-
-static uint32_t	eventcnt;
-static int	val;
-
-static void event_loop(void *arg)
-{
-	uint32_t	event;
-
-printf("Entering event loop\n");
-	while(1) {
-		if(xQueueReceive(_event_queue, &event, portMAX_DELAY)) {
-			//printf("Event: %"PRIu32"\n", event);
-			//printf("eventcnt = %"PRIu32"\n", eventcnt++);
-			if(event)
-				--val;
-			else
-				++val;
-			printf("val = %d\n", val);
-			
-		}
-		//printf("high watermark: %d\n",
-		//    uxTaskGetStackHighWaterMark(NULL));
-	}
-}
-
 
 void
 app_main(void)
@@ -196,12 +276,12 @@ printf("here\n");
 	    "Could not install gpio ISR Service");
 
 	/* Rotary A: input, interrupt */
-	ESP_GOTO_ON_ERROR(config_gpio(GPIO_ROT_A, 1, isr_rotary_a, NULL),
+	ESP_GOTO_ON_ERROR(config_gpio(GPIO_ROT_A, 1, isr_rotary, NULL),
 	    err_label, logtag, "Could not configure pin %d", GPIO_ROT_A);
 
 	/* Rotary B: input, no interrupt */
-	ESP_GOTO_ON_ERROR(config_gpio(GPIO_ROT_B, 1, NULL, NULL), err_label,
-	    logtag, "Could not configure pin %d", GPIO_ROT_B);
+	ESP_GOTO_ON_ERROR(config_gpio(GPIO_ROT_B, 1, isr_rotary, NULL),
+	    err_label, logtag, "Could not configure pin %d", GPIO_ROT_B);
 
 	/* Rotary Switch: input, interrupt */
 	ESP_GOTO_ON_ERROR(config_gpio(GPIO_SWITCH, 1, isr_rotary_switch, NULL),
